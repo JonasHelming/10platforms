a01c61c34ef409895cc63e53355c87388a269c8c
Edgar Mueller <edgar.m.mueller@gmail.com>
2014-10-20 17:31:50 +0200

    Added tabris client

---
+++ client.tabris/src/client/tabris/ExamplePage.java
@@ -0,0 +1,42 @@
+package client.tabris;
+
+
+import org.eclipse.core.databinding.observable.Realm;
+import org.eclipse.emf.ecp.ui.view.ECPRendererException;
+import org.eclipse.jface.databinding.swt.SWTObservables;
+import org.eclipse.rap.rwt.RWT;
+import org.eclipse.rap.rwt.service.UISession;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+
+import client1.App;
+
+import com.eclipsesource.tabris.ui.AbstractPage;
+import com.eclipsesource.tabris.ui.PageData;
+
+public class ExamplePage extends AbstractPage {
+
+	private static final long serialVersionUID = 7800936887572548646L;
+
+	protected void setupRealm(Display display) {
+		UISession uiSession = RWT.getUISession();
+		if (uiSession.getAttribute("realm") == null) {
+			Realm realm = SWTObservables.getRealm(display);
+			RealmSetter.setRealm(realm);
+			RWT.getUISession().setAttribute("realm", realm);
+		}
+	}
+	
+	@Override
+	public void createContent(Composite parent, PageData data) {
+		parent.setLayout(new GridLayout());
+		setupRealm(Display.getCurrent());		
+		try {
+			App.fillShell(parent);
+		} catch (ECPRendererException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ client.tabris/src/client/tabris/TabrisConfiguration.java
@@ -0,0 +1,34 @@
+package client.tabris;
+
+import org.eclipse.rap.rwt.application.Application;
+import org.eclipse.rap.rwt.application.ApplicationConfiguration;
+import org.eclipse.rap.rwt.application.Application.OperationMode;
+
+import com.eclipsesource.tabris.TabrisClientInstaller;
+import com.eclipsesource.tabris.ui.PageConfiguration;
+import com.eclipsesource.tabris.ui.TabrisUIEntrypointFactory;
+import com.eclipsesource.tabris.ui.UIConfiguration;
+
+
+public class TabrisConfiguration implements ApplicationConfiguration {
+
+	public void configure(Application application) {
+		bootstrap(application);
+		application.setOperationMode(OperationMode.SWT_COMPATIBILITY);
+		application.addEntryPoint("/demo", new TabrisUIEntrypointFactory(createUIConfiguration()), null);
+	}
+	
+	private UIConfiguration createUIConfiguration() {
+		UIConfiguration uiConfiguration = new UIConfiguration();
+	    PageConfiguration topLevelPage = new PageConfiguration("topLevelPage", ExamplePage.class);
+	    topLevelPage.setTopLevel(true);
+	    topLevelPage.setTitle("Tabris Demo");
+	    uiConfiguration.addPageConfiguration(topLevelPage);
+	    
+		return uiConfiguration;
+	}
+
+	private void bootstrap(Application application) {
+		TabrisClientInstaller.install(application);
+	}
+}
