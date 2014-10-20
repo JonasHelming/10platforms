cf538b8ed05d4fd426469704ded1d31fec27d3d5
eneufeld <neufeld.eugen@googlemail.com>
2014-10-21 00:22:51 +0200

    fixed fx demo

--- client2.app/src/client2/app/MainApplication.java
+++ client2.app/src/client2/app/MainApplication.java
@@ -1,10 +1,11 @@
 package client2.app;
 
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.stage.Stage;
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
+import org.eclipse.emf.ecp.view.model.fx.ECPFXView;
 import org.eclipse.emf.ecp.view.model.fx.ECPFXViewRenderer;
 import org.eclipse.equinox.app.IApplicationContext;
 import org.eclipse.fx.osgi.util.AbstractJFXApplication;
@@ -14,13 +15,15 @@ import submission.SubmissionFactory;
 
 public class MainApplication extends AbstractJFXApplication {
 	protected void jfxStart(IApplicationContext applicationContext, Application jfxApplication, Stage primaryStage) {
-		Scene s = new Scene(new Label("Hello World"));
+		BorderPane pane=new BorderPane();
+		Scene s = new Scene(pane);
 		primaryStage.setScene(s);
 		primaryStage.setWidth(300);
 		primaryStage.setHeight(400);
 		primaryStage.setTitle("Hello World");
 		Submission submission = SubmissionFactory.eINSTANCE.createSubmission();
-		ECPFXViewRenderer.INSTANCE.render(submission);
+		ECPFXView ecpfxView = ECPFXViewRenderer.INSTANCE.render(submission);
+		pane.setCenter(ecpfxView.getFXNode());
 		primaryStage.show();
 	}
 }
--- targets/efxclipse/plugins/org.eclipse.fx.ide.pde.ui.e4_1.0.0.201408150702/org/eclipse/fx/ide/pde/ui/e4/project/template/NativeLaunchData.xtend
+++
@@ -1,7 +0,0 @@
-package org.eclipse.fx.ide.pde.ui.e4.project.template
-
-@Data class NativeLaunchData {
-	String tychoOutDir;
-	String productName;
-	String vendorName;
-}
