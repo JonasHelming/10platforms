b75e04a713dc6cb2a440d26992f8d1c3c4268eec
jonas <jonas.helming@googlemail.com>
2014-10-24 13:57:55 +0200

    finished demo

--- client2.app/src/client2/app/MainApplication.java
+++ client2.app/src/client2/app/MainApplication.java
@@ -1,10 +1,16 @@
 package client2.app;
 
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
+import org.eclipse.emf.ecp.emf2web.WebHandler;
 import org.eclipse.emf.ecp.view.model.fx.ECPFXView;
 import org.eclipse.emf.ecp.view.model.fx.ECPFXViewRenderer;
 import org.eclipse.equinox.app.IApplicationContext;
@@ -19,11 +25,26 @@ public class MainApplication extends AbstractJFXApplication {
 		Scene s = new Scene(pane);
 		primaryStage.setScene(s);
 		primaryStage.setWidth(300);
-		primaryStage.setHeight(400);
+		primaryStage.setHeight(200);
 		primaryStage.setTitle("Hello World");
-		Submission submission = SubmissionFactory.eINSTANCE.createSubmission();
+		final Submission submission = SubmissionFactory.eINSTANCE.createSubmission();
 		ECPFXView ecpfxView = ECPFXViewRenderer.INSTANCE.render(submission);
 		pane.setCenter(ecpfxView.getFXNode());
+		Button button = new Button();
+		button.setText("Submit");
+		button.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				WebHandler webHandler = new WebHandler("http://localhost:9000");
+				try {
+					webHandler.createWebElement(submission);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+		pane.setBottom(button);
 		primaryStage.show();
 	}
 }
